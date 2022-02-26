b3fbc44c7762963baa80dea0bddd487e9a4ea7f3
Pranav-Kumar-PK <https://github.com/Pranav-Kumar-PK>
2022-02-26 23:42:24 +0530

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,26 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import { ChannelContainer, ChannelListContainer ,Auth} from "./components";
-import './App.css';
+import { ChannelContainer, ChannelListContainer, Auth } from "./components";
+import "./App.css";
+const cookies = new Cookies();
 const apiKey = "dp9m3ppc4u7h";
+const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
-
-const authToken = false;
+if (authToken) {
+  client.connectUser({
+    id: cookies.get("userId"),
+    name: cookies.get("username"),
+    fullName: cookies.get("fullName"),
+    image: cookies.get("avatarURL"),
+    hashedPassword: cookies.get("hashedPassword"),
+    phoneNumber: cookies.get("phoneNumber"),
+  }, authToken);
+}
 
 const App = () => {
-  if(!authToken)return<Auth/>
+  if (!authToken) return <Auth />;
 
   return (
     <div className="app__wrapper">
--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -3,6 +3,7 @@ import Cookies from "universal-cookie";
 import axios from "axios";
 import signinImage from "../assets/signup.jpg";
 
+const cookies = new Cookies();
 
 const initialState = {
   fullName: "",
@@ -29,7 +30,18 @@ const Auth = () => {
     const {data:{token,userId,hashedPassword}} = await axios.post(`${URL}/${isSignup?'signup':'login'}`,{
       username,password,fullName,phoneNumber,avatarURL
     })
+    cookies.set('token', token);
+    cookies.set('username', username);
+    cookies.set('fullName', fullName);
+    cookies.set('userId', userId);
 
+    if(isSignup){
+      cookies.set('phoneNumber', phoneNumber);
+    cookies.set('avatarURL', avatarURL);
+    cookies.set('hashedPassword', hashedPassword);
+    }
+
+    window.location.reload();
   }
   return (
     <div className="auth__form-container">
